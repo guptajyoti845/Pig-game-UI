bb78439fac377d52c2bb09a9f0ef32c28f110126
Jyoti Gupta <jyoti.gupta@INjyotigupta.local>
2019-12-01 14:23:53 +0530

    Add RollDice Functionality and switching the activeplayer on rollDice = 1

--- app.js
+++ app.js
@@ -8,3 +8,46 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores , roundScores , activeplayer , dice;
+scores = [0,0]
+roundScores = 0 ;
+activeplayer = 0 ;
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.dice').style.display = 'block';
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    //Do Something here
+    //1. Random Number
+    dice =  Math.floor(Math.random() * 6) + 1;
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+dice+'.png';
+
+
+    //Update round Score  If rolled number is not 1
+
+    if(dice>1){
+            roundScores += dice;
+            document.querySelector('#current-'+activeplayer).textContent=roundScores;
+    }
+    else{
+        //next Player
+        activeplayer === 0 ? activeplayer=1:activeplayer=0;
+        roundScores=0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'block';
+
+    }
+
+})
+
+
